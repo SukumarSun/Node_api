0bf98888493bef13ed91a45de96db9eab6ef6f6e
sundar-godspeed <sukumar@godspeed.systems>
2023-12-30 05:20:48 +0530

    cors error

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ const corsOptions={
 }
 
 app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://frontend-for-node.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(express.json())
 
 app.use('/api/products', productRoute)
